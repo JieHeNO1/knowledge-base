a27cf54f1752dde79e6eaba227f0b273d184978e
Jie He <41889520+JieHeNO1@users.noreply.github.com>
2025-01-09 17:33:07 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ zhipuai_api_key = "83028078a3e842328c44c2f967645844.EiSeV3wTXgG1ZTBn"
 
 
 def generate_response(input_text, openai_api_key):
-    llm = ChatOpenAI(temperature=0.7, openai_api_key=openai_api_key)
+    llm = ChatOpenAI(temperature=0.7, openai_api_key=openai_api_key,base_url="https://api.gptsapi.net/v1")
     output = llm.invoke(input_text)
     output_parser = StrOutputParser()
     output = output_parser.invoke(output)
@@ -40,7 +40,7 @@ def get_vectordb():
 #带有历史记录的问答链
 def get_chat_qa_chain(question:str,openai_api_key:str):
     vectordb = get_vectordb()
-    llm = ChatOpenAI(model_name = "gpt-4o-mini", temperature = 0,openai_api_key = openai_api_key)
+    llm = ChatOpenAI(model_name = "gpt-4o-mini", temperature = 0,openai_api_key = openai_api_key,base_url="https://api.gptsapi.net/v1")
     memory = ConversationBufferMemory(
         memory_key="chat_history",  # 与 prompt 的输入变量保持一致。
         return_messages=True  # 将以消息列表的形式返回聊天记录，而不是单个字符串
@@ -57,7 +57,7 @@ def get_chat_qa_chain(question:str,openai_api_key:str):
 #不带历史记录的问答链
 def get_qa_chain(question:str,openai_api_key:str):
     vectordb = get_vectordb()
-    llm = ChatOpenAI(model_name = "gpt-4o-mini", temperature = 0,openai_api_key = openai_api_key)
+    llm = ChatOpenAI(model_name = "gpt-4o-mini", temperature = 0,openai_api_key = openai_api_key,base_url="https://api.gptsapi.net/v1")
     template = """使用以下上下文来回答最后的问题。如果你不知道答案，就说你不知道，不要试图编造答
         案。最多使用三句话。尽量使答案简明扼要。总是在回答的最后说“谢谢你的提问！”。
         {context}
